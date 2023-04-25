2d06a7e2b2a4cb5905ca65828183387f1b2ec688
Brian <117960169+brianplus@users.noreply.github.com>
2023-04-25 15:58:14 +0800

    Load UCX libs explicitly (#26)
    * Load UCX libs explicitly once ucx shuffle manager is created
    
    Signed-off-by: Brian Sheng <bins@nvidia.com>
    
    * Add comments for the code change
    
    Signed-off-by: Brian Sheng <bins@nvidia.com>
    
    ---------
    
    Signed-off-by: Brian Sheng <bins@nvidia.com>

--- src/main/scala/org/apache/spark/shuffle/ucx/CommonUcxShuffleManager.scala
+++ src/main/scala/org/apache/spark/shuffle/ucx/CommonUcxShuffleManager.scala
@@ -14,6 +14,7 @@ import org.apache.spark.shuffle.ucx.rpc.UcxRpcMessages.{ExecutorAdded, Introduce
 import org.apache.spark.shuffle.ucx.utils.SerializableDirectBuffer
 import org.apache.spark.util.{RpcUtils, ThreadUtils}
 import org.apache.spark.{SecurityManager, SparkConf, SparkEnv}
+import org.openucx.jucx.NativeLibs
 
 /**
  * Common part for all spark versions for UcxShuffleManager logic
@@ -22,6 +23,11 @@ abstract class CommonUcxShuffleManager(val conf: SparkConf, isDriver: Boolean) e
   type ShuffleId = Int
   type MapId = Int
   type ReduceId = Long
+  
+  /* Load UCX/JUCX libraries as soon as possible to avoid collision with JVM when register malloc/mmap hook. */
+  if (!isDriver) {
+    NativeLibs.load();
+  }
 
   val ucxShuffleConf = new UcxShuffleConf(conf)
 
